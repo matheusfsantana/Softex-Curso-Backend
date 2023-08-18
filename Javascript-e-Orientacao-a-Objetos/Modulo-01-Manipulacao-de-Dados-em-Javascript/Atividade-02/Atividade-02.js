c4e3dd4c0f97f4d7dde1e6ce4bae3d6609d183c2
Matheus Santana <83383321+matheusfsantana@users.noreply.github.com>
2023-08-18 18:28:13 +0000

    JS Modulo-01 Atividade-02

---
+++ Javascript-e-Orientacao-a-Objetos/Modulo-01-Manipulacao-de-Dados-em-Javascript/Atividade-02/Atividade-02.js
@@ -0,0 +1,27 @@
+// Vamos criar um cadastro de pessoa com os seguintes dados: nome, salário, idade e se possui diploma.
+// Antes de começar a codificação, identifiquem os tipos de cada variável. 
+// Em seguida, desenvolvam o código utilizando exemplos para cada variável que respeitem os tipos identificados. 
+// Certifiquem-se de que os valores correspondam aos tipos de dados corretos.
+
+//Nome - String
+//Salario - Number/Float
+//Idade - Number/Int
+//Diploma - Boolean
+
+
+let nome = "Matheus";
+let salario = 15000.00; // um dia
+let idade = 23;
+let diploma = false; //por enquanto rs
+
+
+console.log(nome);
+console.log(salario);
+console.log(idade);
+console.log(diploma);
+
+//verificando os tipos das variaveis
+console.log(typeof(nome));
+console.log(typeof(salario));
+console.log(typeof(idade));
+console.log(typeof(diploma));
