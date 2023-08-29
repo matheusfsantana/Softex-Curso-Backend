9ac8ffae467a4edc89d659db18f9bc63fac97701
Matheus Santana <83383321+matheusfsantana@users.noreply.github.com>
2023-08-29 19:00:07 +0000

    aula exercicio 11

---
+++ Logica-de-Programacao/Exercicios-em-aula/Exercicios/Exercicio-11/Exercicio-11.js
@@ -0,0 +1,67 @@
+// const pessoa = {};
+
+// const pessoa = {
+//   nome: "Matheus",
+// };
+
+// const usuario = {
+//   nome: "Matheus",
+//   idade: 24,
+// };
+
+// console.log(usuario.idade);
+
+// const produto = {
+//   nome: "Sei la",
+//   preco: 50,
+// };
+
+// produto.preco = 75;
+
+// console.log(produto.preco);
+
+// const contato = {
+//   nome: "Fulano",
+//   email: "fulano@fulano",
+// };
+
+// delete contato.email;
+
+// console.log(contato);
+
+// const cliente = {
+//   nome: "Fulano de tal",
+// };
+
+// cliente.endereco = "Rua A, 123";
+
+// console.log(cliente);
+
+// const empresa = {
+//   departamentos: ["A", "B", "C"],
+// };
+
+// console.log(empresa.departamentos[0]);
+
+// const aluno = {
+//   notas: [80, 90, 75],
+// };
+
+// aluno.notas[1] = 95;
+
+// console.log(aluno.notas);
+
+// dados = {
+//    valores: [10, 20, 30],
+// };
+
+// dados.valores.splice(1, 1);
+
+// console.log(dados);
+
+// const teste = {
+//   titulo: "Livro",
+//   autor: "Maria",
+// };
+
+// console.log(teste);
